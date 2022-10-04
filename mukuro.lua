4b3b855613142bf7e8cc13e71688f5d66a2c9b6a
athuria <110605779+athuria@users.noreply.github.com>
2022-10-04 07:57:10 +0700

    Update mukuro.lua

--- mukuro.lua
+++ mukuro.lua
@@ -1,15 +1,15 @@
-repeat wait(0) until game:IsLoaded()
+repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/athuria/mukuro-bf/master/athuriaZ")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BF")()
 elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/athuria/mukuro-kl/master/athuriaZ?token=GHSAT0AAAAAABZLHLUHYF3FZUF2ZOQCJLBSYZVONAA")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/KL")()
 elseif PlaceId == 537413528 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BABFT")()
 elseif PlaceId == 8396586868 or PlaceId == 9432106399 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/AOPG")()
 elseif PlaceId == 914010731 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/athuria/mukuro-roghoul/master/athuriaZ?token=GHSAT0AAAAAABZLHLUG2XFYDPLEFRQU4NYKYZVOLYA")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/Ro-Ghoul")()
 elseif PlaceId == 1730877806 or PlaceId == 3978370137 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/athuria/mukuro-gpo/master/athuriaZ?token=GHSAT0AAAAAABZLHLUGKJVSJRDUM5GMJR2AYZVOJSQ")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/GPO")()
 end
